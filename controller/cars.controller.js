a80f7abd5ca127c025aa2dddcc600377f1b50499
Bilol <83568251+Bilol03@users.noreply.github.com>
2025-11-24 16:40:59 +0500

    post car has been finished

--- controller/cars.controller.js
+++ controller/cars.controller.js
@@ -1,13 +1,42 @@
-const {writeFile, readFile} = require('../utils/fs.js')
+const { writeFile, readFile } = require('../utils/fs.js')
 
 function getCar(req, res) {
-    let data = readFile("./cars.json")
-    return res.json({
-        message: "Success",
-        data
-    })
+	let data = readFile('./cars.json')
+	return res.json({
+		message: 'Success',
+		data,
+	})
 }
 
+function postCar(req, res) {
+	let { model, color, raqam, userId } = req.body
+	if (!(model, color, raqam, userId))
+		return res.json({
+			message: 'All data required',
+		})
+	let users = readFile('./users.json')
+	let user = users.find((el) => el.id == userId)
+	if (!user)
+		return res.json({
+			message: 'User not found in this id',
+		})
+
+	let cars = readFile('./cars.json')
+	let newCar = {
+        id: cars[cars.length - 1] ? cars[cars.length - 1].id + 1 : 1,
+		model,
+		color,
+		raqam,
+		userId,
+	}
+    cars.push(newCar)
+    const message = writeFile("./cars.json", cars)
+    return res.json({
+        message,
+        newCar
+    })
+}
 module.exports = {
-    getCar
+	getCar,
+	postCar,
 }
--- routes/cars.route.js
+++ routes/cars.route.js
@@ -1,6 +1,9 @@
 const express = require('express')
-const {getCar} = require('../controller/cars.controller.js')
+const {getCar, postCar} = require('../controller/cars.controller.js')
 
 const route = express.Router()
+
 route.get('/cars', getCar)
+route.post('/cars', postCar)
+
 module.exports = route
