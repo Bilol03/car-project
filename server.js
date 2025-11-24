2c2c4961f2ce9b53d2f0ecdb3ea6174bafc7a656
Bilol <83568251+Bilol03@users.noreply.github.com>
2025-11-24 16:03:41 +0500

    User create finished

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const userRoute = require('./routes/users.route.js')
+
 
 const app = express()
 app.use(express.json())
 
+
+app.use(userRoute)
 app.listen(8080, () => console.log(8080))
